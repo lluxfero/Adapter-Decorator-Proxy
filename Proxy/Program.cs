fb92109096da28d38a226e4570e1e049a8e07764
Анастасия <onastja02@gmail.com>
2023-05-06 11:52:32 +0300

    proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,42 @@
+﻿RealImage realImage = new();
+realImage.Display();
+Console.WriteLine();
+
+ProxyImage proxyImage = new(realImage);
+proxyImage.Display();
+
+
+public interface IImage
+{
+    void Display();
+}
+
+// Реальный Субъект
+class RealImage : IImage
+{
+    public void Display()
+    {
+        Console.WriteLine("Отображение RealImage..");
+    }
+}
+
+class ProxyImage : IImage
+{
+    private RealImage _realImage;
+
+    public ProxyImage(RealImage realImage)
+    {
+        this._realImage = realImage;
+    }
+
+    public void Display()
+    {
+        this._realImage.Display();
+        this.Logging();
+    }
+
+    public void Logging()
+    {
+        Console.WriteLine($"{DateTime.Now}: RealImage было отображено");
+    }
+}
